9dc42066b3c46b718f7b335949d502e9c4e2dcc8
aliaksandr <litvinaualex@gmail.com>
2023-04-10 17:52:42 +0300

    add login page html, css

---
+++ server.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.responses import Response
+from starlette.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+def index_page():
+    with open('templates/login.html', 'r') as f:
+        login_page = f.read()
+    return Response(login_page, media_type="text/html")

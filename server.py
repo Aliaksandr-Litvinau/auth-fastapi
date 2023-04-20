68dc28de02ad147bb56c33ff473e2e262abafd01
aliaksandr <litvinaualex@gmail.com>
2023-04-20 16:06:07 +0300

    test @app.post("/login")

--- server.py
+++ server.py
@@ -12,3 +12,8 @@ def index_page():
     with open('templates/login.html', 'r') as f:
         login_page = f.read()
     return Response(login_page, media_type="text/html")
+
+
+@app.post("/login")
+def get_success_login():
+    return Response("All right!!!")

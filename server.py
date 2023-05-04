86c4d407a9fcf8785ba53cc85ee996d5eaaa8ed0
aliaksandr <litvinaualex@gmail.com>
2023-05-04 10:36:53 +0300

    add password hash with salt

--- server.py
+++ server.py
@@ -12,6 +12,7 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 SECRET_SIGN_KEY = "88e03b267580d15fc7e239e608a36e643cea2bf86c442b38c6d4b07dccd8d036"
+PASSWORD_SALT = "257e7dfd5017ab0023150e46763a49543c672088f0a9dc18e4a0d69e79e42d1e"
 
 
 def sign_data(data: str) -> str:
@@ -31,16 +32,23 @@ def get_email_from_signed_string(email_signed: str) -> Optional[str]:
         return email
 
 
+def verify_password(email: str, password: str) -> bool:
+    password_hash = hashlib.sha256((password + PASSWORD_SALT).encode()).hexdigest().lower()
+    stored_password_hash = hashlib.sha256((users[email]["password"] + PASSWORD_SALT).encode()).hexdigest().lower()
+    return  password_hash == stored_password_hash
+
+
 # simplified database implementation
 users = {
     "alex_l@gmail.com": {
         "name": "alex",
-        "password": "secure password",
+        "password": "4e6965b2942bf58c3a60bb43e75a99b71d28caadae8d215088af390b3054977f",
+        # hashlib.sha256(("secure password" + PASSWORD_SALT).encode()).hexdigest()
         "balance": 1000_000
     },
     "maria_l@gmail.com": {
         "name": "maria",
-        "password": "very_secure password",
+        "password": "60afe7edbbc5c8e37a591dc8e53ad441dcf670ef68b50c0a138dd3fd43e8c5b4",  # "very secure password"
         "balance": 1000_000_000
     },
 }
@@ -69,7 +77,7 @@ def index_page(email: Optional[str] = Cookie(default=None)):
 @app.post("/login")
 def get_success_login(email: str = Form(...), password: str = Form(...)):
     user = users.get(email)
-    if not user or user["password"] != password:
+    if not user or not verify_password(email, user["password"]):
         return Response("I don't know you", media_type="text/html")
     response = Response(f"Your username: {user['name']},</br> password: {password},</br> balance: {user['balance']}",
                         media_type="text/html")
